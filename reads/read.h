e926d9b17aa99d8dcbfeba4ada56251b4aa72bc0
Allan Roscoche <allanroscoche@gmail.com>
2011-05-16 11:08:41 -0300

    reads in C

---
+++ reads/read.h
@@ -0,0 +1,33 @@
+#ifndef _READ_H_
+#define _READ_H_
+
+typedef Base struct
+{
+  unsigned char b1:2;
+  unsigned char b2:2;
+  unsigned char b3:2;
+  unsigned char b4:2;
+} Base;
+
+
+typedef Read struct
+{
+  unsigned char size; // tamanho do vetor
+  unsigned char begin:4; //inicio no primeiro byte
+  unsigned char end:4; // fim no ultimo byte
+
+  Bases * bases; // cada char tem que guardar 4 bases
+
+} Read;
+
+typedef ReadTable struct
+{
+  unsigned int size;
+  Read * table;
+} ReadTable;
+
+Read * createRead(unsigned char *);
+ReadTable * createTable(void);
+
+
+#endif
