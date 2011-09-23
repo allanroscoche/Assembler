8d667533d4484a86258f310bd40207d66c5b4b1a
Allan Roscoche <allanroscoche@gmail.com>
2011-09-23 16:34:46 -0300

    shift atualizado

--- tools/shift.py
+++ tools/shift.py
@@ -1,17 +1,32 @@
 #!/usr/bin/env python
 
 import sys
+import random
 
 if len(sys.argv) > 3:
     f=open(sys.argv[1],'r')
     seq = ""
     for line in f:
         if line[0] != '>':
-            seq += line
+            seq += line.rstrip('\n')
     size = len(seq)
     chop = int(sys.argv[2])
     shift = int(sys.argv[3])
+
+    saida = [];
     for i in range(0,(size-chop)/shift):
         des = i*shift
-        print seq[des:(des+chop)]
+        saida.append( seq[des:(des+chop)])
+
+    if len(sys.argv)==5 :
+        if sys.argv[4]=="sample" :
+            for linha in saida:
+                print random.choice(saida)
+        elif sys.argv[4]=="full":
+            random.shuffle(saida)
+            for linha in saida:
+                print linha
+    else:
+        for linha in saida:
+            print linha
 
